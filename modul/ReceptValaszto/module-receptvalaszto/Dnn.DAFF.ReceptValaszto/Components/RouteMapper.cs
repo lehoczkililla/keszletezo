511833b0a4476e163ae0fe74107ee88ba7534a04
KatinkaVanyak <vanyakkatinka@gmail.com>
2023-05-24 11:46:12 +0200

    RouteMapper, Api

---
+++ modul/ReceptValaszto/module-receptvalaszto/Dnn.DAFF.ReceptValaszto/Components/RouteMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetNuke.Web.Api;
+using System.Diagnostics;
+
+namespace DAFF.Modules.Dnn.DAFF.ReceptValaszto.Components
+{
+    public class RouteMapper : IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute(
+               "ReceptValaszto",
+               "default",
+               "{controller}/{action}",
+               new string[] { "DAFF.Modules.Dnn.DAFF.ReceptValaszto.Controllers.Api" });
+        }
+    }
+}
