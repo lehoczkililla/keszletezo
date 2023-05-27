02ac936a83651837d5e756c0e338d72438767f4e
mEszter2 <reka.debreczeni@stud.uni-corvinus.hu>
2023-05-27 20:19:20 +0200

    regexek hozzaadasa

--- keszletezo2/Hova.cs
+++ keszletezo2/Hova.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace keszletezo2
@@ -20,12 +15,23 @@ namespace keszletezo2
         public void pathBox_Validating(object sender, CancelEventArgs e)
         {
             //nézzük meg, hogy megadták-e az elérési utat
-            if (!ellenoriz(pathBox.Text))
+            if (string.IsNullOrEmpty(pathBox.Text))
             {
                 e.Cancel = true;
                 // ha nem, írjuk ki a hibát
                 errorProvider1.SetError(pathBox, "Kérem adja meg az elérési utat!");
             }
+            if (!string.IsNullOrEmpty(pathBox.Text) && !Regex.IsMatch(pathBox.Text, "/"))
+            {
+                e.Cancel= true;
+                errorProvider1.SetError(pathBox, "Kérem előre perjeleket (/) használjon!");
+            }
+            if (!string.IsNullOrEmpty(pathBox.Text) && Regex.IsMatch(pathBox.Text, "/") && !Regex.IsMatch(pathBox.Text, "/$"))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(pathBox, "Kérem az elérési út végére is tegyen perjelet!");
+            }
+
         }
 
         public void pathBox_Validated(object sender, EventArgs e)
